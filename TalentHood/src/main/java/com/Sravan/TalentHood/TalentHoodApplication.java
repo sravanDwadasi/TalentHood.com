f6a5d1b4bad649fb1586666f4a9cc11916176a20
sravanDwadasi <sravandwadasi@gmail.com>
2025-03-20 15:52:41 +0530

    Added springboot project

---
+++ TalentHood/src/main/java/com/Sravan/TalentHood/TalentHoodApplication.java
@@ -0,0 +1,13 @@
+package com.Sravan.TalentHood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentHoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentHoodApplication.class, args);
+	}
+
+}
---
+++ TalentHood/src/test/java/com/Sravan/TalentHood/TalentHoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sravan.TalentHood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentHoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
